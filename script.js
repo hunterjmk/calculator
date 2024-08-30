f956e3bc0e70f6fe332c528e6bafe0e80e6a8d61
hunterjmk <136958839+hunterjmk@users.noreply.github.com>
2024-08-30 14:47:34 +0300

    Create arithmetic functions

---
+++ script.js
@@ -0,0 +1,25 @@
+/* 
+Create functions for the following items and testing them in your browser’s console.
+add
+subtract
+multiply
+divide
+
+*/
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
